9f7f777e74fbf8011f2b8d4640cc971d845fab4b
Kylie <35279443+kyliecho@users.noreply.github.com>
2024-03-07 18:13:04 +0800

    Add files via upload

---
+++ Encoder.java
@@ -0,0 +1,72 @@
+import java.util.Random;
+
+public class Encoder {
+    private Random random;
+
+    public Encoder() {
+        this.random = new Random();
+    }
+
+    public char encodeChar(char c, int offset) {
+        int refIndex = Parser.charToRefIndex(c);
+        
+        // check if character is in reference table
+        if (refIndex == -1) {
+            return c;
+        }
+
+        // shift character
+        int newRefIndex = refIndex - offset;
+
+        // wrap around
+        if (newRefIndex<0) {
+            newRefIndex += 44;
+        }
+        return Parser.refIndexToChar(newRefIndex);
+    }
+
+    public char decodeChar(char c, int offset) {
+        int refIndex = Parser.charToRefIndex(c);
+        
+        // check if character is in reference table
+        if (refIndex == -1) {
+            return c;
+        }
+
+        // shift character
+        int newRefIndex = refIndex + offset;
+
+        // wraparound
+        if (newRefIndex>43) {
+            newRefIndex -= 44;
+        }
+        return Parser.refIndexToChar(newRefIndex);
+    }
+
+    public String encode(String plainText) {
+        // choose random offset character
+        int offset = random.nextInt(44);
+
+        // generate encoded text
+        char offsetChar = Parser.refIndexToChar(offset);
+        String encodedText = "" + offsetChar;
+        for (int i = 0; i<plainText.length(); i++) {
+            char curr = plainText.charAt(i);
+            encodedText += encodeChar(curr, offset);
+        }
+        return encodedText;
+    }
+
+    public String decode(String encodedText) {
+        // identify offset 
+        int offset = Parser.getOffset(encodedText);
+
+        // generate plain text
+        String plainText = "";
+        for (int i = 1; i<encodedText.length(); i++) {
+            char curr = encodedText.charAt(i);
+            plainText += decodeChar(curr, offset);
+        }
+        return plainText;
+    }
+}
---
+++ Parser.java
@@ -0,0 +1,46 @@
+public class Parser {
+    // takes in a character, returns its index in the reference table
+    public static int charToRefIndex(char c) {
+        int i = (int)c;
+        int diff;
+        if (i>=40 && i<=47) {
+            // ()*+,-./
+            diff = 4;
+        } else if (i>=48 && i<=57) {
+            // 0-9
+            diff = 22;
+        } else if (i>=65 && i<=90) {
+            // A-Z
+            diff = 65;
+        } else {
+            // not in reference table
+            return -1;
+        }
+        int refIndex = i - diff;
+        return refIndex;
+    }
+
+    // takes in an index in the reference table, returns the corresponding character
+    public static char refIndexToChar(int i) {
+        int diff;
+        if (i<=25) {
+            // A-Z
+            diff = 65;
+        } else if (i>=36) {
+            // ()*+,-./
+            diff = 4;
+        } else {
+            // 0-9
+            diff = 22;
+        }
+        int ascii = i+diff;
+        return (char)ascii;
+    }
+
+    // extracts the offset character from encoded text
+    public static int getOffset(String text) {
+        char offsetChar = text.charAt(0);
+        int offset = charToRefIndex(offsetChar);
+        return offset;
+    }
+}
